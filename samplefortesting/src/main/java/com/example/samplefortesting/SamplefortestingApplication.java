ade271d156f97b68817138437e9cd5c98db8c592
mkumarreddy <mkumarreddy@ENS-HLHKZH3>
2024-03-15 19:40:46 +0530

    sample  testing for github

---
+++ samplefortesting/src/main/java/com/example/samplefortesting/SamplefortestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplefortesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplefortestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplefortestingApplication.class, args);
+	}
+
+}
---
+++ samplefortesting/src/test/java/com/example/samplefortesting/SamplefortestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplefortesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplefortestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
